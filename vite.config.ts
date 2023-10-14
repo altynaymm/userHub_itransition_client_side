d0b838da237639a4391a806084535fc8626351d4
Altynay Muratkyzy <125205125+altynaymm@users.noreply.github.com>
2023-10-15 01:16:47 +0600

    bug

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(
+  {
+    server: {
+      proxy: {
+        '/api': 'https://userhub-itransition-db40c4fa7fa7.herokuapp.com/'
+      }
+    }
+  }
+  ,{
   plugins: [react()],
 })
