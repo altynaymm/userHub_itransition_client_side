35bf1fb9e7f660d097ce1fa6c873b6ef3c2acfbe
Altynay Muratkyzy <125205125+altynaymm@users.noreply.github.com>
2023-10-14 13:32:16 +0600

    app. jsx fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,17 @@ import { Route, Routes } from 'react-router-dom'
 import SignUp from './components/SignUp/SignUp'
 import SignIn from './components/SignIn/SignIn'
 import UsersList from './components/UsersList/UsersList'
-import { checkSession } from '../redux/Thunk/user.thunk'
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import Home from './components/Home/Home'
 
 
 function App() {
 
   return (
-
     <>
       <Routes>
-        <Route path='/sign-up' element={<SignUp/>}></Route>
-        <Route path='/sign-in' element={<SignIn/>}></Route>
-        <Route path='/users' element={<UsersList/>}> </Route>
+        <Route path='/' element={<SignIn />}> </Route>
+        <Route path='/sign-up' element={<SignUp />}></Route>
+        <Route path='/users' element={<UsersList />}> </Route>
       </Routes>
     </>
   )
