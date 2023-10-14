5e018f68dcdb3b061f825d80e8bea0482579fdd2
Altynay Muratkyzy <125205125+altynaymm@users.noreply.github.com>
2023-10-14 13:32:56 +0600

    final changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Route, Routes } from 'react-router-dom'
 import SignUp from './components/SignUp/SignUp'
 import SignIn from './components/SignIn/SignIn'
 import UsersList from './components/UsersList/UsersList'
-import Home from './components/Home/Home'
+
 
 
 function App() {
